5aa0e71600a6accb680164b5e0ca125dab5ca9af
Yossi Yadgar <yosyad@gmail.com>
2016-07-23 16:23:18 +0300

    first version

---
+++ Assignment1.py
@@ -0,0 +1,19 @@
+import numpy
+from matplotlib import pyplot as plt
+import math
+
+n = 1000
+m = 40
+
+xMatrix = numpy.random.rand(n, m) * 100
+bParametersVec = numpy.random.rand(m, 1) * 5
+noiseVec = numpy.random.rand(n, 1)
+yVec = xMatrix.dot(bParametersVec) + noiseVec
+bEstimated = numpy.l
+inalg.inv(xMatrix.transpose().dot(xMatrix)).dot(xMatrix.transpose().dot(yVec))
+differenceNorma = numpy.linalg.matrix_power(numpy.linalg.norm(bParametersVec - bEstimated, axis=1), 2)
+
+print(differenceNorma)
+
+plt.plot(differenceNorma)
+plt.show()
