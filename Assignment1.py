b98fd8878ab819d128b7ac489ffaf1592aa60f8b
Yossi Yadgar <yosyad@gmail.com>
2016-07-23 16:35:26 +0300

    first version - togeter

--- Assignment1.py
+++ Assignment1.py
@@ -10,7 +10,7 @@ bParametersVec = numpy.random.rand(m, 1) * 5
 noiseVec = numpy.random.rand(n, 1)
 yVec = xMatrix.dot(bParametersVec) + noiseVec
 bEstimated = numpy.l
-inalg.inv(xMatrix.transpose().dot(xMatrix)).dot(xMatrix.transpose().dot(yVec))
+numpy.linalg.inv(xMatrix.transpose().dot(xMatrix)).dot(xMatrix.transpose().dot(yVec))
 differenceNorma = numpy.linalg.matrix_power(numpy.linalg.norm(bParametersVec - bEstimated, axis=1), 2)
 
 print(differenceNorma)
